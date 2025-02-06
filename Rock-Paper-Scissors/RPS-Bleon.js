d11aab094a3f5703e15abc8249bb88cc02becc5a
Bleon5--Github--Username <bleonarllati2@gmail.com>
2025-02-06 18:40:49 +0100

    RPS/Bleon finished

---
+++ Rock-Paper-Scissors/RPS-Bleon.js
@@ -0,0 +1,23 @@
+const rpsChoice = process.argv.slice(2);
+
+const randomTextGenerator = ["Rock", "Paper", "Scissors"];
+const randomIndGenerator = Math.floor(
+  Math.random() * randomTextGenerator.length
+);
+
+const compChoice = randomTextGenerator[randomIndGenerator];
+const userChoice = rpsChoice[0].toLowerCase();
+
+if (userChoice === "rock" && compChoice === "Scissors") {
+  console.log(`You win, You chose ${userChoice}, computer chose ${compChoice}`);
+} else if (userChoice === "paper" && compChoice === "Rock") {
+  console.log(`You win, You chose ${userChoice}, computer chose ${compChoice}`);
+} else if (userChoice === "scissors" && compChoice === "Paper") {
+  console.log(`You win, You chose ${userChoice}, computer chose ${compChoice}`);
+} else if (userChoice === compChoice.toLowerCase()) {
+  console.log(`It's a tie! Both chose ${userChoice}`);
+} else {
+  console.log(
+    `You lose, You chose ${userChoice}, computer chose ${compChoice}`
+  );
+}
