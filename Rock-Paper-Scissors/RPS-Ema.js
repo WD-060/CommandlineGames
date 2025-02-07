e3f8253dc5f64b7a9600d4674e444b7d0d892446
scie <emabicova@seznam.cz>
2025-02-07 10:15:42 +0100

    finished RPS

--- Rock-Paper-Scissors/RPS-Ema.js
+++ Rock-Paper-Scissors/RPS-Ema.js
@@ -1,26 +1,29 @@
-const args = process.argv.slice(2);
+const RPS = () => {
+    const args = process.argv.slice(2);
 
+    const game = function() {
+        const moves = ['rock', 'paper', 'scissors'];
+        const decimalRandom = Math.random() * moves.length; // e.g. 0.21 * 3 = 0.63
+        const integerRandom = Math.floor(decimalRandom); // 2
+        let computer = moves[integerRandom];
+        let player = args[0];
 
-const game = function() {
-    const moves = ['rock', 'paper', 'scissors'];
-    let computer = moves[Math.random() * moves.length];
-    let player = moves[];
-    if (computer === player) {
-        return `The computer chose ${computer}. You also chose ${player}. We have a draw.`;
-    } else if (computer == 'paper' && player == 'rock' || computer == 'scissors' && player == 'paper' || computer == 'rock' && player = 'scissors') {
-        return `The computer chose ${computer}. You chose ${player}. The computer wins.`;
-    } else if (computer == 'rock' && player == 'paper' || computer == 'paper' && player == 'scissors' || computer == 'scissors' && player = 'paper') {
-        return `The computer chose ${computer}. You chose ${player}. You win.`;
-    } else {
-        return `Something has gone horribly wrong.`
-    };    
+        // RANDOM r p s    terminal
+        // console.log(computer, player) - was checking if choices work
+        if (computer === player) {
+            console.log(`The computer chose ${computer}. You also chose ${player}. We have a draw.`);
+        } else if ((computer == 'paper' && player == 'rock') || (computer == 'scissors' && player == 'paper') || (computer == 'rock' && player == 'scissors')) {
+            console.log(`The computer chose ${computer}. You chose ${player}. The computer wins.`);
+        } else if ((computer == 'rock' && player == 'paper') || (computer == 'paper' && player == 'scissors') || (computer == 'scissors' && player == 'rock')) {
+            console.log(`The computer chose ${computer}. You chose ${player}. You win.`);
+        } else {
+            console.log(`Something has gone horribly wrong.`);
+        }
     };
 
+    game()
+}
 
 
-// let player;
-// let computer;
+RPS()
 
-// if (player = 'rock' == true && computer = 'paper' == true) {
-//     return `Fuck you.`
-// };
