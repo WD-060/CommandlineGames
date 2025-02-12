463e342990ca8d14dbe029729a57e74974dba906
Abdulwahed Bin Mothana <abdulwahed.mothana@yahoo.com>
2025-02-12 13:57:37 +0100

    my projects

--- English-To-Pig-Latin/ETPL-Abdulwahed.js
+++ English-To-Pig-Latin/ETPL-Abdulwahed.js
@@ -1,15 +1,15 @@
-function toPigLatin(word) {     //The function with one  argument.
+function toPigLatin(word) {     
 
-    const vowels = "aeiou"; // A string "aeiou" is stored in the variable vowels for easy lookup.
-    if (vowels.includes(word[0].toLowerCase())) {  //word[0] check first letter of the word. vowels.includes(...) checks if the first letter is a vowel.
+    const vowels = "aeiou"; 
+    if (vowels.includes(word[0].toLowerCase())) {  
         return word + "way";
     }
     else {
     let i = 0;
-    while (i < word.length && !vowels.includes(word[i].toLowerCase())) { //Find the first vowel in the word
+    while (i < word.length && !vowels.includes(word[i].toLowerCase())) { 
         i++;
     }
-    return word.slice(i) + word.slice(0, i) + "ay"; //gets the part of the word from the first vowel to the end.and gets the leading consonants (before the first vowel).
+    return word.slice(i) + word.slice(0, i) + "ay"; 
 }
 }
 console.log(toPigLatin("hello")); // ellohay
