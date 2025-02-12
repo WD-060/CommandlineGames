0e3392fdc1e73a9059c6661ea87cc099a2e23d3a
Vegtamskvitha <73757161+Vegtamskvitha@users.noreply.github.com>
2025-02-12 13:33:46 +0100

    Fixed some typos

--- Caesar-Cipher/CaeCi-vegtamskvitha.js
+++ Caesar-Cipher/CaeCi-vegtamskvitha.js
@@ -1,4 +1,6 @@
-//Brauche bessere Erklärung; sorgt dafür, daß ein Key von 27 einfach als Key von 1 ausgeführt wird, sprich, es "wrapped around"
+const args = process.argv.slice(2); 
+
+//Brauche bessere Erklärung; sorgt dafür, daß ein Key von 27 einfach als Key von 1 ausgeführt wird, sprich, es "wrapped around",
 //Wie die Erläuterung es nannte. Der Modulo handhabt die Reste von Divisionen, die hierbei eine Rolle spielen. Weiterhin unklar.
 function mod(n, p)
 {
@@ -12,19 +14,19 @@ function mod(n, p)
 
 function EnactEnigma(message, key) {
 
-  let encodedMessage = "";  //Okay, das sollte das Ergebnis ganz am Ende speichern
+  let encodedMessage = "";  //Okay, das sollte das Ergebnis ganz am Ende speichern.
   //const upperCaseMessage = message.toUpperCase() //Funktioniert so nicht...
 
   for (let i = 0; i < message.length; i++) {
 
-        let code = message.charCodeAt(i); //Gibt den Unicode (sprich, Buchstaben als Zahlen innerhalb eines bestimmten Systems - nachlesen)
+        let code = message.charCodeAt(i); //Gibt den Unicode (sprich, Buchstaben als Zahlen innerhalb eines bestimmten Systems - nachlesen).
 
-        //Nur A-Z, Lateinisches Alphabet ohne Sonderzeichen oder -buchstaben - damit bestimmen wir, welche Teile des Unicodealphabets wir verwenden
+        //Nur A-Z, Lateinisches Alphabet ohne Sonderzeichen oder -buchstaben - damit bestimmen wir, welche Teile des Unicodealphabets wir verwenden.
         if (code >= 65 && code <= 65 + 26 - 1)
         {
-            code -= 65;
-            code = mod(code + key, 26); //und hierfür haben wir mod definiert;immer noch nicht ganz sicher, wie genau die Mathematik dahiner funktioniert
-            code += 65;
+            code -= 65; //Wenn das Symbol außerhalb des Unicodes liegt, machen wir - nichts.
+            code = mod(code + key, 26); //und hierfür haben wir mod definiert;immer noch nicht ganz sicher, wie genau die Mathematik dahinter funktioniert. Sorgt aber letztenendes dafür, daß, zusammen mit dem Key, das Alphabet verschoben wird.
+            code += 65; //Wenn das Symbol außerhalb des Unicodes liegt, machen wir - nichts.
         }
 
         encodedMessage += String.fromCharCode(code);
